74edf2d8d1047ba7736511c5fc3cbaa6c8097a33
Evaldas Grudzinskas <prohdo@icloud.com>
2016-01-14 19:54:13 +0200

    Add categories raw controller with CRUD

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,16 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/edit'
+
+  get 'categories/new'
+
+  get 'categories/show'
+
+  get 'categories/delete'
+
   root 'application#index'
 end
